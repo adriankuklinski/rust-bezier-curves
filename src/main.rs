d773bc738932d7ad5c2be071e32e0842036bc42f
Adrian Kuklinski <adriankuklinski@pm.me>
2023-10-09 18:05:43 -0400

    feat: create blank window on app start

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
-use bevy::prelude::*;
-
+use bevy::{
+    prelude::*,
+    window::WindowMode,
+};
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    fit_canvas_to_parent: true,
+                    mode: WindowMode::BorderlessFullscreen,
+                    ..default()
+                }),
+                ..default()
+            }),
+        )
+        .run();
 }
-
