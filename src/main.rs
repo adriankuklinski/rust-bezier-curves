ce5070d1cc6858f9c1f7c7f5f323c93b95b9a487
Adrian Kuklinski <adriankuklinski@pm.me>
2023-10-09 23:24:13 -0400

    feat: create and add setup system

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,44 @@
 use bevy::{
     prelude::*,
-    window::WindowMode,
+    window::{Window, WindowMode},
 };
+
+use std::collections::HashMap;
+
+#[derive(Resource)]
+struct PointMaterial(Handle<ColorMaterial>);
+
+#[derive(Component, Clone, Debug)]
+struct Point {
+    position: Vec3,
+    level: u32,
+}
+
+#[derive(Resource)]
+struct Line {
+    start: Entity,
+    end: Entity,
+    level: u32,
+}
+
+#[derive(Default, Resource)]
+struct BezierData {
+    points: HashMap<Entity, Point>,
+    lines: HashMap<Entity, Line>,
+    current_level: u32,
+}
+
+fn setup(
+    mut commands: Commands,
+    mut bezier_data: ResMut<BezierData>,
+    mut materials: ResMut<Assets<ColorMaterial>>
+) {
+    let point_material = materials.add(Color::rgb(1.0, 0.0, 0.0).into());
+    commands.insert_resource(PointMaterial(point_material));
+    commands.spawn(Camera2dBundle::default());
+    bezier_data.current_level = 0;
+}
+
 fn main() {
     App::new()
         .add_plugins(
@@ -14,5 +51,8 @@ fn main() {
                 ..default()
             }),
         )
+        .insert_resource(BezierData::default())
+        .add_systems(Startup, setup)
         .run();
 }
+
